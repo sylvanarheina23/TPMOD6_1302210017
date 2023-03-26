1508580fabcdb5861d4204737934662d73403c38
sylvanarheina23 <127048576+sylvanarheina23@users.noreply.github.com>
2023-03-26 16:55:55 +0700

    menambahkan class SayaTubeVideo

--- TPMOD6_1302210017/Program.cs
+++ TPMOD6_1302210017/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design by Contract [Sylvana Rheina Kharmeliawaty]");
+        sayaTubeVideo.PrintVideoDetails();
+    }
+}
+
---
+++ TPMOD6_1302210017/SayaTubeVideo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rnd = new Random();
+        this.title = title;
+        this.id = rnd.Next(10000, 99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("===Video Details===");
+        Console.WriteLine("Video ID\t: " + this.id);
+        Console.WriteLine("Title\t\t: " + this.title);
+        Console.WriteLine("Play Count\t: " + this.playCount);
+    }
+}
