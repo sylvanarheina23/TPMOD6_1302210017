a5bc420060bf61604ef002103678b383cc4ce507
sylvanarheina23 <127048576+sylvanarheina23@users.noreply.github.com>
2023-03-26 20:00:45 +0700

    final

--- TPMOD6_1302210017/SayaTubeVideo.cs
+++ TPMOD6_1302210017/SayaTubeVideo.cs
@@ -1,46 +1,40 @@
-﻿
-using System;
+﻿using System;
 using System.Diagnostics.Contracts;
 
 public class SayaTubeVideo
-{ 
+{
     private int id;
     private string title;
     private int playCount;
 
     public SayaTubeVideo(string title)
     {
-            Random rnd = new Random();
-            this.title = title;
-            this.id = rnd.Next(10000, 99999);
         Contract.Requires(title.Length < 100);
         Contract.Requires(title != null);
         this.title = title;
-            var random = new Random();
-            this.id = random.Next(10000, 99999);
-            this.playCount = 0;
+        var random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.playCount = 0;
     }
-
-        public void IncreasePlayCount(int playCount)
         public void IncreasePlayCount(int count)
+        { 
+        Contract.Requires(count <= 10000000);
+        try
         {
-            this.playCount += playCount;
-            Contract.Requires(count <= 10000000);
-            try
-            {
-                this.playCount = checked(this.playCount + count);
-            }
-            catch(OverflowException e)
-            {
-                Console.WriteLine("Overflow Exception : " + e.Message);
-            }
+            this.playCount = checked(this.playCount + count);
         }
-
-    public void PrintVideoDetails()
-    {
-        Console.WriteLine("===Video Details===");
-        Console.WriteLine("Video ID\t: " + this.id);
-        Console.WriteLine("Title\t\t: " + this.title);
-        Console.WriteLine("Play Count\t: " + this.playCount);
+        catch (OverflowException e)
+        {
+            Console.WriteLine("Overflow Exception : " + e.Message);
+        }
     }
+
+
+public void PrintVideoDetails()
+{
+    Console.WriteLine("===Video Details===");
+    Console.WriteLine("Video ID\t: " + this.id);
+    Console.WriteLine("Title\t\t: " + this.title);
+    Console.WriteLine("Play Count\t: " + this.playCount);
 }
+} 
