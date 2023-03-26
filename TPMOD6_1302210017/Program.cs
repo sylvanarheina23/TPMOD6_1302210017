f0d140ec49400e8507280e970e044513e57191a4
sylvanarheina23 <127048576+sylvanarheina23@users.noreply.github.com>
2023-03-26 17:12:45 +0700

    implementasi design by contract

--- TPMOD6_1302210017/Program.cs
+++ TPMOD6_1302210017/Program.cs
@@ -1,12 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 public class Program
 {
     public static void Main(string[] args)
     {
         SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design by Contract [Sylvana Rheina Kharmeliawaty]");
+        sayaTubeVideo.IncreasePlayCount(500);
         sayaTubeVideo.PrintVideoDetails();
+        sayaTubeVideo.IncreasePlayCount(2147483647);
     }
 }
+
 
--- TPMOD6_1302210017/SayaTubeVideo.cs
+++ TPMOD6_1302210017/SayaTubeVideo.cs
@@ -1,23 +1,40 @@
-﻿using System;
+﻿
+using System;
+using System.Diagnostics.Contracts;
 
 public class SayaTubeVideo
-{
+{ 
     private int id;
     private string title;
     private int playCount;
 
     public SayaTubeVideo(string title)
     {
-        Random rnd = new Random();
+            Random rnd = new Random();
+            this.title = title;
+            this.id = rnd.Next(10000, 99999);
+        Contract.Requires(title.Length < 100);
+        Contract.Requires(title != null);
         this.title = title;
-        this.id = rnd.Next(10000, 99999);
-        this.playCount = 0;
+            var random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.playCount = 0;
     }
 
-    public void IncreasePlayCount(int playCount)
-    {
-        this.playCount += playCount;
-    }
+        public void IncreasePlayCount(int playCount)
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += playCount;
+            Contract.Requires(count <= 10000000);
+            try
+            {
+                this.playCount = checked(this.playCount + count);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("Overflow Exception : " + e.Message);
+            }
+        }
 
     public void PrintVideoDetails()
     {
